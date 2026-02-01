7f7c68df1de60f4f6545a46c77ad507131777327
NJ03-010011 <3791571617@qq.com>
2026-02-01 21:53:29 +0800

    New！！！

---
+++ src/main/java/com/kunming/decorative/KRTDecorativeItemsClient.java
@@ -0,0 +1,10 @@
+package com.kunming.decorative;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KRTDecorativeItemsClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
